f1de9c3344d51860003301bdf28b86a21a8d76b4
LucasHarim <harimlgc@usp.br>
2024-10-28 15:48:23 -0300

    movendo employee_scheduler pra pasta experimental/

--- tractian_hackathon/__init__.py
+++
@@ -1,8 +0,0 @@
-from .employee_scheduler import (
-    Employee,
-    Task,
-    fetch_employees,
-    fetch_tasks,
-    record_history,
-    get_suitable_employees_for_task,
-    optimize_schedule_tasks)
