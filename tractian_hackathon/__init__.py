f0cd62319fa614252215a67e56101eb78bd12f81
LucasHarim <harimlgc@usp.br>
2024-10-28 15:45:12 -0300

    Adicionando employee_scheduler.py

---
+++ tractian_hackathon/__init__.py
@@ -0,0 +1,8 @@
+from .employee_schedule import (
+    Employee,
+    Task,
+    fetch_employees,
+    fetch_tasks,
+    record_history,
+    get_suitable_employees_for_task,
+    optimize_schedule_tasks)
