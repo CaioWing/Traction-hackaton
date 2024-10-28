eeb01c0c22876548babec25bfe237ddfbb826684
Vinicius Formigone <formiga5557@gmail.com>
2024-10-28 13:43:32 -0300

    Implementado fastapi ao openai

--- api/main.py
+++ api/main.py
@@ -1,16 +1,6 @@
-from fastapi import FastAPI
-import pymongo
 
 
-myclient = pymongo.MongoClient("mongodb://localhost:27017/")
-mydb = myclient["Gearing"]
 
 
-app = FastAPI()
 
 
-@app.get("/orderServices/{item_id}")
-def read_item(item_id: int):
-    mycol = mydb["serviceOrders"]
-    x = mycol.find()
-    return {"item_id": x}
