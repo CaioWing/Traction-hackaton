d9ce9a4fdd5a2dd795853653296dbc6e5aa474c9
Vinicius Formigone <formiga5557@gmail.com>
2024-10-28 12:43:39 -0300

    Mongodb connection added

--- api/main.py
+++ api/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+import pymongo
+
+
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+mydb = myclient["Gearing"]
+
+
 app = FastAPI()
 
 
 @app.get("/orderServices/{item_id}")
 def read_item(item_id: int):
-    return {"item_id": item_id}
+    mycol = mydb["serviceOrders"]
+    x = mycol.find()
+    return {"item_id": x}
